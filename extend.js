7024dfaad3e8f5566f9e67913ed04c6888ae376a
shuzhen <sue10th@hotmail.com>
2013-10-15 19:46:09 +0800

    extend done.

---
+++ extend.js
@@ -0,0 +1,30 @@
+var extend = function(properties) {
+
+var parent = this;
+var child;
+
+//setting up a child
+if (properties && _.has(properties, 'constructor')){ 
+	child = properties.constructor;
+	}
+	
+//no prototype supplied		
+	else{
+	child = function(){ 
+		return parent.apply(this, arguments);
+		}; //apply chains the constructors for an object
+}
+
+_.extend(child, parent); //puts all parent properties in to child
+
+child.prototype = parent.prototype;
+
+if(properties) //if child properties are given
+_.extend(chid.prototype, properties);
+
+child.__super__ = parent.prototype;
+
+return child;
+};
+
+Model.extend = Collection.extend = Router.extend = View.extend = History.extend = extend;
